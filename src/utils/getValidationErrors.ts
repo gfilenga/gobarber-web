acb3ade9f9a6ed45b348bad826a1a1e1b52f7b98
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2020-11-03 09:00:31 -0300

    Exibindo erros no input

---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,16 @@
+import { ValidationError } from 'yup';
+
+// Dessa forma permite que a interface aceite qualquer parâmetro que seja string
+interface Errors {
+  [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    validationErrors[error.path] = error.message;
+  });
+
+  return validationErrors;
+}
